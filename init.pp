43d00f4b509a3ba6ff27efbe32c732f8d920e1e2
ManasaTanmayi <31545070+ManasaTanmayi@users.noreply.github.com>
2017-09-01 11:50:29 -0500

    User

---
+++ init.pp
@@ -0,0 +1,7 @@
+#This Module Will Create an User in the Linux Operating System And Downloads some packages for the user created from internet
+
+class user {
+include user::creation
+include user::downloads
+}
+
