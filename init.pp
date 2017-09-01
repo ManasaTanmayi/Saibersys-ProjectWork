4dc86cc3aa6065ef85a1991dfde6dbaf752c8997
ManasaTanmayi <31545070+ManasaTanmayi@users.noreply.github.com>
2017-09-01 13:38:33 -0500

    Delete init.pp

--- init.pp
+++
@@ -1,7 +0,0 @@
-#This Module Will Create an User in the Linux Operating System And Downloads some packages for the user created from internet
-
-class user {
-include user::creation
-include user::downloads
-}
-
